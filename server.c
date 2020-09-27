914df9f9dcc762824f8f73b766d0d4761fffeb16
vladislv31 <vladislv.k31@gmail.com>
2020-09-27 13:20:53 +0300

    bind, listen

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <errno.h>
+#include <netinet/in.h>
 
 
 int main()
@@ -13,7 +14,34 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    
+    struct sockaddr_in address = {0};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(8888);
+
+    int server_bind = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+
+    if (server_bind < 0) {
+        perror("cant bind socket");
+        exit(EXIT_FAILURE);
+    }
+
+    int server_listen = listen(server_fd, 10);
+
+    if (server_listen < 0) {
+        perror("listen error");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
