74d8981dc15e1b54b0ebc02af64dd2d156b448be
vladislv31 <vladislv.k31@gmail.com>
2020-09-27 13:11:27 +0300

    create socket

--- server.c
+++ server.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <errno.h>
 
 
 int main()
 {
-    //
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd < 0) {
+        perror("cant create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    
 
     return 0;
 }
